0a98fe6f21aac7ecf297d015c0aa35ef1cbe9250
Harish Purushothaman <harish@nexthop.ai>
2026-05-29 11:27:28 -0700

    Update report formatter

--- formatter.py
+++ formatter.py
@@ -3,10 +3,10 @@ def build_report(on_track, at_risk, overdue):
     at_risk_count = len(at_risk)
     overdue_count = len(overdue)
 
-    total_report = """ IT WEEKLY STATUS REPORT
-                ON TRACK: {on_track_count} tickets
-                AT RISK: {at_risk_count} tickets
-                OVERDUE: {overdue_count} tickets
+    total_report = f""" IT WEEKLY STATUS REPORT
+ON TRACK: {on_track_count} tickets
+AT RISK: {at_risk_count} tickets
+OVERDUE: {overdue_count} tickets
     """
 
     return total_report
